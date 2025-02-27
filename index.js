2fccaa6683e6b320c589c7c0215cf71eecacd772
Fuad Jamal <jamalfuad34@gmail.com>
2025-02-27 13:09:07 +0200

    this is the main file

---
+++ index.js
@@ -0,0 +1,13 @@
+import postLength, { findPost } from './post.js'
+
+// console.log('crash course')
+// console.log(global)
+// console.log(process)
+
+// const {randomNumbers, temperature} = require('./util')
+
+// console.log(`Random: ${randomNumbers()}`)
+// console.log(`Temperature: ${temperature(30)}`)
+
+console.log(findPost())
+console.log(postLength())
