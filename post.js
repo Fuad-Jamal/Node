15367e119c30decacac059b48ecf8c01b6d4b342
Fuad Jamal <jamalfuad34@gmail.com>
2025-02-27 13:10:03 +0200

    file that contains exported functions

---
+++ post.js
@@ -0,0 +1,14 @@
+const posts = [
+    {id: 1, title: 'Post 1'},
+    {id: 2, title: 'Post 2'},
+    {id: 3, title: 'Post 3'}
+]
+
+export const findPost = ()=> {
+    return posts
+};
+
+const postLength = () => {
+    return posts.length
+}
+export default postLength;
