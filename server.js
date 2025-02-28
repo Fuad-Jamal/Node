7d6aaa9c095bfb54f2ba8cf567513efa7631f2b7
Fuad Jamal <jamalfuad34@gmail.com>
2025-02-28 10:10:20 +0200

    adding the status code and changing the port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = 3000;
 const server = http.createServer((req,res) => {
     res.setHeader('Content-Type', 'text/html')
     res.statusCode = 404;
+    console.log(req.url)
+    console.log(req.method)
     res.write('<h1>Send IT</h1>')
     res.end()
 })
