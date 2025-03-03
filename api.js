4ba8d95ca2dc37cc8a4ec5c51e44f0f5f4fa85a7
Fuad Jamal <jamalfuad34@gmail.com>
2025-03-03 13:10:10 +0200

    adding routers to api

--- api.js
+++ api.js
@@ -5,11 +5,35 @@ const users = [
     {id: 1, name: 'John Doe', age: 25},
     {id: 2, name: 'Jane Doe', age: 24}
 ]
+const createUser = (req,res) => {
+    let body = '';
+    req.on('data', (chunk) =>{
+        body += chunk.toString()
+    })
+    req.on('end', () =>{
+        const newUser = JSON.parse(body)
+        users.push(newUser)
+        res.statusCode = 201
+        res.write(JSON.stringify(newUser))
+        res.end()
+    })
+}
 
 const server = createServer((req,res) => {
     if(req.url === '/api/users' && req.method === 'GET'){
         res.setHeader('Content-Type', 'application/json')
         res.write(JSON.stringify(users))
+        res.end()
+    }else if(req.url.match(/\api\/users\/([0-9]+)/)&& req.method === 'GET'){
+        res.setHeader('Content-Type', 'application/json')
+        res.write(JSON.stringify({id: 1, name: 'John Doe', age: 25}))
+        res.end()
+    }else if(req.url === '/api/users' && req.method === 'POST'){
+        createUser(req,res)
+    }else{
+        res.setHeader('Content-Type', 'text/html')
+        res.statusCode = 404;
+        res.write('<h1>Route Not Found</h1>')
         res.end()
     }
 })
