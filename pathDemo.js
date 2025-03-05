c4f63dec245c852801d497a69f07eafaedc53e13
Fuad Jamal <jamalfuad34@gmail.com>
2025-03-05 09:49:21 +0200

    joining paths to create a dileminars

--- pathDemo.js
+++ pathDemo.js
@@ -1,5 +1,13 @@
-import path from   'path';
+import path from 'path';
 
 const file = './dir1/dir2/text.txt'
 
 console.log(path.basename(file))
+
+console.log(path.extname(file))
+
+console.log(path.parse(file))
+
+__dirname
+let files = (path.join(__dirname, 'dir1', 'dir2', 'text.txt'))
+console.log(files)
